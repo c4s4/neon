188654b9d0a2061733b64093cef2cae242979a37
Michel Casabianca <casa@sweetohm.net>
2017-01-19 15:03:20 +0100

    Added link task

--- src/neon/build/task.go
+++ src/neon/build/task.go
@@ -29,6 +29,7 @@ func init() {
 		"chdir":  Chdir,
 		"mkdir":  MkDir,
 		"touch":  Touch,
+		"link":   Link,
 		"remove": Remove,
 		"delete": Delete,
 		"if":     If,
@@ -152,6 +153,37 @@ func Touch(target *Target, args util.Object) (Task, error) {
 					return fmt.Errorf("creating file '%s': %v", path, err)
 				}
 			}
+		}
+		return nil
+	}, nil
+}
+
+func Link(target *Target, args util.Object) (Task, error) {
+	fields := []string{"link", "to"}
+	if err := CheckFields(args, fields, fields); err != nil {
+		return nil, err
+	}
+	s, err := args.GetString("link")
+	if err != nil {
+		return nil, fmt.Errorf("argument link must be a string")
+	}
+	d, err := args.GetString("to")
+	if err != nil {
+		return nil, fmt.Errorf("argument to of task link must be a string")
+	}
+	return func() error {
+		source, err := target.Build.Context.ReplaceProperties(s)
+		if err != nil {
+			return fmt.Errorf("processing link argument: %v", err)
+		}
+		dest, err := target.Build.Context.ReplaceProperties(d)
+		if err != nil {
+			return fmt.Errorf("processing to argument of link task: %v", err)
+		}
+		fmt.Printf("Linking file '%s' to '%s'\n", source, dest)
+		err = os.Symlink(source, dest)
+		if err != nil {
+			return fmt.Errorf("linking files: %v", err)
 		}
 		return nil
 	}, nil
