74ff89805a900d6ea19bfdc84bc37215c85c66f0
Michel Casabianca <casa@sweetohm.net>
2017-02-02 11:57:58 +0100

    Added directory and filename builtin functions

---
+++ src/neon/builtin/directory.go
@@ -0,0 +1,26 @@
+package builtin
+
+import (
+	"neon/build"
+	"path/filepath"
+)
+
+func init() {
+	build.BuiltinMap["directory"] = build.BuiltinDescriptor{
+		Function: Directory,
+		Help: `Return directory of a given path.
+
+Arguments:
+- The path to get directory for as a string.
+Returns:
+- The directory of the path as a string.
+
+Examples:
+// get directory of path "/foo/bar/spam.txt"
+dir = directory("/foo/bar/spam.txt")`,
+	}
+}
+
+func Directory(path string) string {
+	return filepath.Dir(path)
+}
