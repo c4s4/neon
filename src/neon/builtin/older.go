3681746e2b493338f2b2106be20d55e2020f885e
Michel Casabianca <casa@sweetohm.net>
2017-07-31 20:27:56 +0200

    Added older builtin function

---
+++ src/neon/builtin/older.go
@@ -0,0 +1,44 @@
+package builtin
+
+import (
+	"neon/build"
+	"os"
+)
+
+func init() {
+	build.BuiltinMap["older"] = build.BuiltinDescriptor{
+		Function: Older,
+		Help: `Tells if source is older than result file (if any).
+
+Arguments:
+
+- source: source file that must exist.
+- result: result file (may not exist).
+
+Returns:
+
+- A boolean that tells if source is older tha result. If result file doesn't
+  exists, this returns true.
+
+Examples:
+
+    // generate PDF if source Markdown changed
+    if older("source.md", "result.pdf") {
+    	compile("source.md")
+    }`,
+	}
+}
+
+func Older(source, result string) bool {
+	info, err := os.Stat(source)
+	if err != nil {
+		panic("could no get info about source file")
+	}
+	sourceTime := info.ModTime()
+	info, err = os.Stat(result)
+	if err != nil {
+		return true
+	}
+	resultTime := info.ModTime()
+	return sourceTime.After(resultTime)
+}
