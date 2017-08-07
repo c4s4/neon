efdd75d831ad958c2dc8ca1d435a89864a455da3
Michel Casabianca <casa@sweetohm.net>
2017-08-07 10:44:23 +0200

    Fixed sleep task to accept int or float durations

--- src/neon/task/sleep.go
+++ src/neon/task/sleep.go
@@ -14,12 +14,14 @@ func init() {
 		
 Arguments:
 
-- sleep: the duration to sleep in seconds as an integer.
+- sleep: the duration to sleep in seconds as a float or integer.
 
 Examples:
 
-    # sleep for 10 seconds
-    - sleep: 10`,
+    # sleep for 1.5 seconds
+    - sleep: 1.5
+    # sleep for 3 seconds
+    - sleep: 3`,
 	}
 }
 
@@ -28,12 +30,17 @@ func Sleep(target *build.Target, args util.Object) (build.Task, error) {
 	if err := CheckFields(args, fields, fields); err != nil {
 		return nil, err
 	}
-	duration, ok := args["sleep"].(int)
-	if !ok {
-		return nil, fmt.Errorf("argument of task sleep must be an integer")
+	var duration float64
+	switch time := args["sleep"].(type) {
+	case int:
+		duration = float64(time)
+	case float64:
+		duration = time
+	default:
+		return nil, fmt.Errorf("argument of task sleep must be a float or an int")
 	}
 	return func() error {
-		build.Message("Sleeping for %d seconds...", duration)
+		build.Message("Sleeping for %g seconds...", duration)
 		time.Sleep(time.Duration(duration) * time.Second)
 		return nil
 	}, nil
