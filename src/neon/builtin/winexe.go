221446ff4b3bc014411881b165c49378a62c0dce
Michel Casabianca <casa@sweetohm.net>
2017-08-11 11:24:58 +0200

    Fix executable path in winexe

--- src/neon/builtin/winexe.go
+++ src/neon/builtin/winexe.go
@@ -12,6 +12,7 @@ func init() {
 		Help: `Add '.exe' or '.bat' extensions depending on platform:
 - command will stay command on Unix and will become command.exe on Windows.
 - script.sh will stay script.sh on Unix and will become script.bat on Windows.
+It will also replace / with \ in the executable path.
 
 Arguments:
 
@@ -24,8 +25,8 @@ Returns:
 Examples:
 
     // run command foo on unix and windows
-    run(winexe("foo"))
-    // will run foo on unix and foo.exe on windows
+    run(winexe("bin/foo"))
+    // will run bin/foo on unix and bin\foo.exe on windows
     // run script script.sh unix and windows
     run(winexe("script.sh"))
     // will run script.sh on unix and script.bat on windows`,
@@ -33,17 +34,14 @@ Examples:
 }
 
 func Winexe(command string) string {
-	if strings.HasSuffix(command, ".sh") {
-		if util.Windows() {
-			return command[:len(command)-3] + ".bat"
+	if util.Windows() {
+		if strings.HasSuffix(command, ".sh") {
+			command = command[:len(command)-3] + ".bat"
 		} else {
-			return command
+			command = command + ".exe"
 		}
+		return strings.Replace(command, "/", "\\", -1)
 	} else {
-		if util.Windows() {
-			return command + ".exe"
-		} else {
-			return command
-		}
+		return command
 	}
 }
