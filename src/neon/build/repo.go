77b79ba1ce1efc5662898a8e3f8fb505f297b9d9
Michel Casabianca <casa@sweetohm.net>
2018-02-09 11:55:04 +0100

    Added template discovery, so that you can call template 'django' instead of 'c4s4/build/django.tpl' for instance

--- src/neon/build/repo.go
+++ src/neon/build/repo.go
@@ -7,6 +7,7 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"sort"
 	"strings"
 )
 
@@ -65,14 +66,51 @@ func PrintParents(repo string) {
 	}
 }
 
-// PrintTemplates prints templates in repository:
+// FindTemplates finds templates in given repository.
 // - repo: the NeON repository (defaults to '~/.neon')
-func PrintTemplates(repo string) {
+// Return:
+// - list of template files relative to repo.
+// - error if something went wrong.
+func FindTemplates(repo string) ([]string, error) {
 	if repo == "" {
 		repo = DefaultRepo
 	}
 	repo = util.ExpandUserHome(repo)
 	files, err := util.FindFiles(repo, []string{"*/*/*.tpl"}, nil, false)
+	if err != nil {
+		return nil, err
+	}
+	sort.Strings(files)
+	return files, nil
+}
+
+// FindTemplates finds a template in given repository.
+// - template: the template to find (such as "golang").
+// - repo: the NeON repository (defaults to '~/.neon')
+// Return:
+// - templates path relative to repo (such as "c4s4/build/golang.tpl").
+// - error if something went wrong.
+func FindTemplate(template, repo string) ([]string, error) {
+	files, err := FindTemplates(repo)
+	if err != nil {
+		return nil, err
+	}
+	var templates []string
+	for _, file := range files {
+		start := strings.LastIndex(file, "/") + 1
+		end := strings.LastIndex(file, ".")
+		name := file[start:end]
+		if name == template {
+			templates = append(templates, file)
+		}
+	}
+	return templates, nil
+}
+
+// PrintTemplates prints templates in repository:
+// - repo: the NeON repository (defaults to '~/.neon')
+func PrintTemplates(repo string) {
+	files, err := FindTemplates(repo)
 	if err != nil {
 		panic(err)
 	}
