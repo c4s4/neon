07dcaf6ad017323ef372eb483e75bc5336499eff
Michel Casabianca <casa@sweetohm.net>
2018-02-21 15:12:50 +0100

    Fixed golint warning

--- src/neon/builtin/absolute_test.go
+++ src/neon/builtin/absolute_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestAbsolute(t *testing.T) {
-	actual := util.PathToUnix(Absolute("foo/../bar/spam.txt"))
+	actual := util.PathToUnix(absolute("foo/../bar/spam.txt"))
 	if !strings.HasSuffix(actual, "bar/spam.txt") {
 		t.Errorf("TestAbsolute failed")
 	}
