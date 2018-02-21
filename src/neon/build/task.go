d691d3948db7c82c44f70ffab7da84024b62e128
Michel Casabianca <casa@sweetohm.net>
2018-02-21 18:33:13 +0100

    Passed fmt

--- src/neon/build/task.go
+++ src/neon/build/task.go
@@ -294,7 +294,7 @@ func evaluateExpression(field reflect.StructField, val interface{}, context *Con
 	if actual != expected {
 		// we accept if expected is slice of interfaces and actual is slice
 		if !(expected.Kind() == reflect.Slice && actual.Kind() == reflect.Slice) &&
-		// or if slice and wrap
+			// or if slice and wrap
 			!(expected == reflect.SliceOf(actual) && FieldIs(field, FieldWrap)) {
 			return nil, fmt.Errorf("bad expression return type, expected '%v' but '%v' was returned",
 				expected, actual)
