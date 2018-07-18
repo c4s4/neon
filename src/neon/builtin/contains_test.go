c2890894394f774d1b08f6e47308bb72e0a83554
Michel Casabianca <casa@sweetohm.net>
2018-07-18 23:36:40 +0200

    Added coverage on contains

--- src/neon/builtin/contains_test.go
+++ src/neon/builtin/contains_test.go
@@ -17,4 +17,12 @@ func TestContains(t *testing.T) {
 	if contains([]interface{}{"foo", "bar"}, "spam") {
 		t.Errorf("Error builtin contains")
 	}
+	var content interface{}
+	content = []string{"foo", "bar"}
+	if !contains(content, "bar") {
+		t.Errorf("Error builtin contains")
+	}
+	if contains(content, "spam") {
+		t.Errorf("Error builtin contains")
+	}
 }
