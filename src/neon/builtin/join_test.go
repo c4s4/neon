70ff46e004096183ae3d9da2fd7714d8b851f349
Michel Casabianca <casa@sweetohm.net>
2018-07-24 22:19:39 +0200

    Improved join coverage

--- src/neon/builtin/join_test.go
+++ src/neon/builtin/join_test.go
@@ -8,4 +8,25 @@ func TestJoin(t *testing.T) {
 	if join([]string{"foo", "bar"}, " ") != "foo bar" {
 		t.Errorf("Error builtin join")
 	}
+	if join([]interface{}{"foo", "bar"}, " ") != "foo bar" {
+		t.Errorf("Error builtin join")
+	}
+}
+
+func TestJoinPanic(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	join(1, " ")
+}
+
+func TestJoinPanic2(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	join([]int{1}, " ")
 }
