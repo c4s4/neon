c383c62a72e8b379151ff5127d04ff35f5a1e59e
Michel Casabianca <casa@sweetohm.net>
2018-08-31 20:35:03 +0200

    Added coverage on build

--- src/neon/build/build_test.go
+++ src/neon/build/build_test.go
@@ -1,9 +1,47 @@
 package build
 
 import (
+	"os"
+	"reflect"
 	"runtime"
 	"testing"
 )
+
+func TestNewBuild(t *testing.T) {
+	// write the build file
+	buildFile := `doc: Test build file
+
+properties:
+    FOO: 'foo'
+
+targets:
+
+    test:
+        doc: Test target
+        steps:
+        - test: 'This is a test'`
+	WriteFile("/tmp", "build.yml", buildFile)
+	defer os.Remove("/tmp/build.yml")
+	// define test task
+	TaskMap = make(map[string]TaskDesc)
+	type testArgs struct {
+		Test string
+	}
+	AddTask(TaskDesc{
+		Name: "test",
+		Func: testFunc,
+		Args: reflect.TypeOf(testArgs{}),
+		Help: `Task documentation.`,
+	})
+	// load the build file
+	build, err := NewBuild("/tmp/build.yml", "/tmp", "")
+	if err != nil {
+		t.Errorf("Error parsing build file: %v", err)
+	}
+	if build.Dir != "/tmp" {
+		t.Errorf("Bad build dir: %s", build.Dir)
+	}
+}
 
 func TestGetShell(t *testing.T) {
 	build := &Build{
