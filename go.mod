2da5249dbbed473ee7a9a0f10599181ea25f7e23
Michel Casabianca <casa@sweetohm.net>
2018-10-30 11:30:26 +0100

    Added chagelog task

--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/c4s4/neon
 
 require (
-	github.com/fatih/color v1.7.1-0.20180516100307-2d684516a886
-	github.com/mattn/anko v0.0.7-0.20180829024344-59cc36605347
+	github.com/c4s4/changelog v1.6.0
+	github.com/fatih/color v1.7.0
+	github.com/mattn/anko v0.0.6
 	github.com/mattn/go-colorable v0.0.9 // indirect
 	github.com/mattn/go-isatty v0.0.4 // indirect
 	github.com/mattn/go-zglob v0.0.0-20180803001819-2ea3427bfa53
---
+++ task/changelog.go
@@ -0,0 +1,67 @@
+package task
+
+import (
+	"fmt"
+	"github.com/c4s4/neon/build"
+	"reflect"
+	"github.com/c4s4/changelog/lib"
+)
+
+func init() {
+	build.AddTask(build.TaskDesc{
+		Name: "changelog",
+		Func: changelog,
+		Args: reflect.TypeOf(changelogArgs{}),
+		Help: `Load information from semantic changelog file.
+
+Arguments:
+
+- changelog: the name of the changelog file (look for changelog in current
+  directory if omitted).
+
+Note:
+
+- The release version is stored in property _changelog_version.
+- The release date is stored in property _changelog_date.
+- The release summary is stored in property _changelog_summary.
+
+Examples:
+
+    # get changelog information in file 'test.yml':
+    - changelog: "test.yml"`,
+	})
+}
+
+type changelogArgs struct {
+	Changelog string `neon:"file,optional"`
+}
+
+func changelog(context *build.Context, args interface{}) error {
+	params := args.(changelogArgs)
+	var file string
+	if params.Changelog == "" {
+		var err error
+		file, err = lib.FindChangelog()
+		if err != nil {
+			return err
+		}
+	} else {
+		file = params.Changelog
+	}
+	source, err := lib.ReadChangelog(file)
+	if err != nil {
+		return err
+	}
+	changelog, err := lib.ParseChangelog(source)
+	if err != nil {
+		return err
+	}
+	if len(changelog) < 1 {
+		return fmt.Errorf("the changelog is empty")
+	}
+	release := changelog[0]
+	context.SetProperty("_changelog_version", release.Version)
+	context.SetProperty("_changelog_date", release.Date)
+	context.SetProperty("_changelog_summary", release.Summary)
+	return nil
+}
